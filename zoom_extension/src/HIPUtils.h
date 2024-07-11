0a9c10a0e3675fb6ec524e76714c799ede08ddf3
123epsilon <arhammkhan@gmail.com>
2024-07-11 22:26:09 +0000

    add various indexing kernels

---
+++ zoom_extension/src/HIPUtils.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ZoomContext.h"
+
+namespace at::zoom {
+
+// Check if every tensor in a list of tensors matches the current
+// device.
+inline bool check_device(ArrayRef<Tensor> ts) {
+  if (ts.empty()) {
+    return true;
+  }
+  Device curDevice = Device(kPrivateUse1, c10::zoom::current_device());
+  for (const Tensor& t : ts) {
+    if (t.device() != curDevice) return false;
+  }
+  return true;
+}
+
+} // namespace at::zoom
